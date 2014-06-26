5f078407b334119a8cee953411bb98581431ad52
Benjamin Blumberg <blumbeb@cobalt.com>
2014-06-26 17:38:48 +0000

    added JsonParser

---
+++ src/main/java/com/testing123/vaadin/JsonParser.java
@@ -0,0 +1,39 @@
+package com.testing123.vaadin;
+
+import java.io.File;
+import java.io.IOException;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+ 
+public class JsonParser {
+    public static void main(String[] args) {
+ 
+	JsonClass folder = new JsonClass();
+	ObjectMapper mapper = new ObjectMapper();
+ 
+	try {
+ 
+		// convert user object to json string, and save to a file
+		mapper.writeValue(new File("c:\\user.json"), folder);
+ 
+		// display to console
+		System.out.println(mapper.writeValueAsString(folder));
+ 
+	} catch (JsonGenerationException e) {
+ 
+		e.printStackTrace();
+ 
+	} catch (JsonMappingException e) {
+ 
+		e.printStackTrace();
+ 
+	} catch (IOException e) {
+ 
+		e.printStackTrace();
+ 
+	}
+ 
+  }
+ 
+}
