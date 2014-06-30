c01430059acda7605af5c29ce5d9dd8b40aaee83
Benjamin Blumberg <blumbeb@cobalt.com>
2014-06-30 19:53:12 +0000

    Can handle multiple maps

--- src/main/java/com/testing123/vaadin/MapToJsonArray.java
+++ src/main/java/com/testing123/vaadin/MapToJsonArray.java
@@ -3,43 +3,68 @@ package com.testing123.vaadin;
 import java.util.Arrays;
 import java.util.Map;
 
-public class MapToJsonArray<K,V>{
-	
+public class MapToJsonArray<K, V> {
+
 	private static String[][] dataPoints;
-	
-	private static <K,V> void makeJavaArray(Map<K,V> map){
-		
+	private static Data[] labledDataPoints;
+
+	private static <K, V> void makeJavaArray(Map<K, V> map) {
+
 		int size = map.size();
 		dataPoints = new String[size][2];
-		
+
 		int count = 0;
-		for(Map.Entry<K, V> entry : map.entrySet()){
-		    dataPoints[count][0] =  entry.getKey().toString();
-		    dataPoints[count][1] = entry.getValue().toString();
-		    count++;
+		for (Map.Entry<K, V> entry : map.entrySet()) {
+			dataPoints[count][0] = entry.getKey().toString();
+			dataPoints[count][1] = entry.getValue().toString();
+			count++;
 		}
 	}
-	
-	public static <K,V> String mapToString(Map<K, V> map){
+
+	public static <K, V> String mapToString(Map<K, V> map) {
 		makeJavaArray(map);
 		return "[" + Arrays.deepToString(dataPoints) + "]";
 	}
-	
-	private static class Data{
-		private double firstVal;
-		private double secondVal;
-		private String source;
-		
-		public Data(double one, double two, String src){
-			firstVal=one;
-			secondVal=two;
-			source=src;
+
+	public static <K, V> void makeJavaArray(Map<K, V> map1, Map<K, V> map2) {
+		int size = map1.size();
+		if (size != map2.size()) {
+			throw new IllegalArgumentException("maps have different size");
 		}
-		
-		@Override
-		public String toString(){
-			return "["+firstVal+","+secondVal+",\""+source+"\"]"; 
+		labledDataPoints = new Data[size];
+
+		int count = 0;
+		for (K key : map1.keySet()) {
+			if (map1.containsKey(key) && map2.containsKey(key)) {
+				String first = map1.get(key).toString();
+				String second = map2.get(key).toString();
+				String src = key.toString();
+				labledDataPoints[count] = new Data(first, second, src);
+			}
+			count++;
 		}
 	}
-	
+
+	public static <K, V> String mapToString(Map<K, V> map1, Map<K, V> map2) {
+		makeJavaArray(map1, map2);
+		return "[" + Arrays.deepToString(labledDataPoints) + "]";
+	}
+
+	private static class Data {
+		private String firstVal;
+		private String secondVal;
+		private String source;
+
+		public Data(String one, String two, String src) {
+			firstVal = one;
+			secondVal = two;
+			source = src;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + firstVal + "," + secondVal + ",\"" + source + "\"]";
+		}
+	}
+
 }
