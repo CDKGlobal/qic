da6390282a9a8dd6f7efb1303be8e91a88d485a9
Weiyou Dai <weiyoud@cobalt.com>
2014-07-02 21:17:30 +0000

    Msr test

---
+++ src/test/java/com/testing123/vaadin/MsrTest.java
@@ -0,0 +1,34 @@
+package com.testing123.vaadin;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MsrTest {
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+    }
+
+    @Test
+    public void testSetKeyAndGetKey() {
+        Msr msr = new Msr();
+        msr.setKey("");
+        assertEquals("", msr.getKey());
+    }
+
+    @Test
+    public void testSetFrmt_valAndGetFrmt_val() {
+        Msr msr = new Msr();
+        msr.setFrmt_val("0.0");
+        assertEquals("0.0", msr.getFrmt_val());
+    }
+
+    @Test
+    public void testSetValAndGetVal() {
+        Msr msr = new Msr();
+        msr.setVal(0.0);
+        assertEquals(0, (int) msr.getVal());
+    }
+}
