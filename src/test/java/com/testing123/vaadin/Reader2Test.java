a8c13e480cd947c2334ba272b63ae179c656ef7e
Christopher Chen <chenc@cobalt.com>
2014-07-02 20:07:29 +0000

    added new test

---
+++ src/test/java/com/testing123/vaadin/Reader2Test.java
@@ -0,0 +1,33 @@
+package com.testing123.vaadin;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Reader2Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void TestReadNonValidMetricReturnsEmptySet() {
+		assertEquals(0, Reader2.getData(null, "").size());
+		assertEquals(0, Reader2.getData("", "").size());
+		assertEquals(0, Reader2.getData("", null).size());
+		assertEquals(0, Reader2.getData(null, null).size());
+		assertEquals(0, Reader2.getData("ncloc", " ").size());
+		assertEquals(0, Reader2.getData("fee", "foo").size());
+	}
+	
+	@Test
+	public void TestReadTwoMetricReturnsNonEmptySet() {
+		assertNotEquals(0, Reader2.getData("ncloc", "complexity").size());
+		assertNotEquals(0, Reader2.getData("complexity", "comment_lines").size());
+	}
+	
+	
+}
