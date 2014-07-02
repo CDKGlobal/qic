f91bdfdc5644a34c68081392ff1c2515702ee941
Christopher Chen <chenc@cobalt.com>
2014-07-02 21:26:37 +0000

    added documentation

--- src/main/java/com/testing123/vaadin/CallFolders.java
+++ src/main/java/com/testing123/vaadin/CallFolders.java
@@ -1,28 +1,32 @@
 package com.testing123.vaadin;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 /**
- * Runs API calls in threads
+ * Runs API calls in threads, in order to speed up the time it takes to accumulate the files
  * @author chenc
  *
  */
 @SuppressWarnings("serial")
 public class CallFolders extends RecursiveTask<MapHolder> {
-	
 	private int low;
 	private int high;
 	private MapHolder m;
 	private List<WebData> fL;
 	private static final int SEQUENTIAL_CUTOFF = 50;
 	
+	/**
+	 * Constructs a new CallFolders object so the Fork-Join framework can use it
+	 * 
+	 * @param low the folder cutoff
+	 * @param high the folder cutoff
+	 * @param m the MapHolder to store the data at the end
+	 * @param fL the list of folders to be opened
+	 */
 	public CallFolders(int low, int high, MapHolder m, List<WebData> fL) {
 		this.low = low;
 		this.high = high;
@@ -30,43 +34,52 @@ public class CallFolders extends RecursiveTask<MapHolder> {
 		this.fL = fL;
 	}
 	
+	/**
+	 * Runs the program in parallel
+	 */
 	@Override
 	protected MapHolder compute() {
+		// intuitively marks each folder from 1...n
+		// if the difference in the current portion of folders is greater than the cutoff,
+		// it will compute this function linearly.
 		if (high - low <= SEQUENTIAL_CUTOFF) {
 			for (int i = low; i < high; i++) {
 				try {
+					// Gets the current folder being opened and prepares the url
 					WebData folder = fL.get(i);
 					String currentFolder = "http://sonar.cobalt.com/api/resources?resource="
 							+ folder.getKey()
 							+ "&depth=1&" 
-							+ "metrics=" + Reader2.metric1 + "," + Reader2.metric2
-							+ "&format=json";
+							+ "metrics=" + Reader2.metric1 + "," + Reader2.metric2 + "&format=json";
 					//System.out.println(currentFolder);
+					
+					// puts all the files in that folders into a list of WebData
 					URL filesLink = new URL(currentFolder);
 					List<WebData> fileList;
 					fileList = Reader2.mapper.readValue(filesLink, new TypeReference<List<WebData>>() {});
 					
+					// Goes through the list of all the files and creates a DataPoint to put into a map
 					for (WebData file : fileList) {
 						if (fileList.size() != 0) {
 							if (!file.getQualifier().equals("CLA")) {
+								// continues if the current file is not a java file
 								continue;
 							} else if (m.fileData.contains(file.getName())) {
+								// appends a string to the end of the file name if another file
+								// with the same name already exists
 								file.setName(file.getName().trim() + " (2)");
 							}
 							m.fileData.add(new DataPoint(file.getName(), file.getMsr().get(1).getVal(), 
 															file.getMsr().get(0).getVal()));
 						}
 					}
-				} catch (JsonParseException e) {
-					e.printStackTrace();
-				} catch (JsonMappingException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 			return m;
 		} else {
+			// Splits the current computation into two threads.
 			int mid = low + (high - low) / 2;
 			CallFolders left = new CallFolders(low, mid, m, fL);
 			CallFolders right = new CallFolders(mid, high, m, fL);
--- src/main/java/com/testing123/vaadin/DataPoint.java
+++ src/main/java/com/testing123/vaadin/DataPoint.java
@@ -1,44 +1,94 @@
 package com.testing123.vaadin;
 
+/**
+ * DataPoint represents an object that contains a file's information.  It allows this data
+ * to be stored so it can be easily plotted onto a graph
+ * 
+ */
 public class DataPoint {
     private String key;
     private double complexity;
     private double lineOfCode;
 
+    /**
+     * Constructs a new DataPoint object with specified parameters
+     * 
+     * @param key the key of the file
+     * @param complexity the complexity of the file
+     * @param lineOfCode the number of lines of code in the file
+     */
     public DataPoint(String key, double complexity, double lineOfCode) {
         this.key = key;
         this.complexity = complexity;
         this.lineOfCode = lineOfCode;
     }
 
+    /**
+     * Constructs a null DataPoint object that represents a file that does not exist.
+     */
     public DataPoint() {
         this(null, -1, -1);
     }
 
+    /**
+     * Sets the key of the file
+     * 
+     * @param key the string representing the key
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * Sets the complexity of the file
+     * 
+     * @param complexity a double value representing the file's complexity
+     */
     public void setComplexity(double complexity) {
         this.complexity = complexity;
     }
 
+    /**
+     * Sets the number of lines of code of the file
+     * 
+     * @param lineOfCode a double value representing the number of lines of code
+     */
     public void setLineOfCode(double lineOfCode) {
         this.lineOfCode = lineOfCode;
     }
 
+    /**
+     * Returns the key of the file
+     * 
+     * @return key
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Returns the complexity of the file
+     * 
+     * @return complexity
+     */
     public double getComplexity() {
         return complexity;
     }
 
+    /**
+     * Returns the number of lines of code in the file
+     * 
+     * @return lineOfCode
+     */
     public double getLineOfCode() {
         return lineOfCode;
     }
 
+    /**
+     * Returns a string representation of this object.
+     * 
+     * @return a string representing this DataPoint
+     */
     @Override
     public String toString() {
         return "[" + lineOfCode + ", " + complexity + ", \"" + key + " \"]";
