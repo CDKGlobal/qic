fca57f3be21ec435b67c713b8a2ce947ccd7da1c
Weiyou Dai <weiyoud@cobalt.com>
2014-07-02 23:52:48 +0000

    Changed the order of expected and actual values and added getName test

--- src/test/java/com/testing123/vaadin/TestJsonParser.java
+++ src/test/java/com/testing123/vaadin/TestJsonParser.java
@@ -1,6 +1,6 @@
 package com.testing123.vaadin;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.net.URL;
 import java.util.List;
@@ -12,46 +12,52 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestJsonParser {
-	static WebData one;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		URL link = new URL("http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:platform&depth=2&metrics=ncloc&format=json")
-				.toURI().toURL();
+    static WebData one;
 
-		List<WebData> jsonClassList = mapper.readValue(link, new TypeReference<List<WebData>>() {});
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        URL link = new URL("http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:platform&depth=2&metrics=ncloc&format=json")
+        .toURI().toURL();
 
-		one = jsonClassList.get(0);
-	}
+        List<WebData> jsonClassList = mapper.readValue(link, new TypeReference<List<WebData>>() {});
 
-	@Test
-	public void testGetId() {
-		assertEquals(one.getId(), 17737);
-	}
-	
-	@Test
-	public void testGetKey() {
-		assertEquals(one.getKey(), "com.cobalt.dap:platform:com.cobalt.dap.wicket.view.packageattribute.GeoTargetingEditPage");
-	}
-	
-	@Test
-	public void testGetScope() {
-		assertEquals(one.getScope(), "FIL");
-	}
-	
-	@Test
-	public void testGetQualifier() {
-		assertEquals(one.getQualifier(), "CLA");
-	}
+        one = jsonClassList.get(0);
+    }
 
-	@Test
-	public void testGetLName() {
-		assertEquals(one.getLname(), "com.cobalt.dap.wicket.view.packageattribute.GeoTargetingEditPage");
-	}
-	
-	@Test
-	public void testGetMSR() {
-		assertEquals((int)one.getMsr().get(0).getVal(), 591);
-	}
+    @Test
+    public void testGetId() {
+        assertEquals(17737, one.getId());
+    }
+
+    @Test
+    public void testGetKey() {
+        assertEquals("com.cobalt.dap:platform:com.cobalt.dap.wicket.view.packageattribute.GeoTargetingEditPage", one.getKey());
+    }
+
+    @Test
+    public void testGetScope() {
+        assertEquals("FIL", one.getScope());
+    }
+
+    @Test
+    public void testGetQualifier() {
+        assertEquals("CLA", one.getQualifier());
+    }
+
+    @Test
+    public void testGetLName() {
+        assertEquals("com.cobalt.dap.wicket.view.packageattribute.GeoTargetingEditPage", one.getLname());
+    }
+
+    @Test
+    public void testGetMSR() {
+        assertEquals(591, (int)one.getMsr().get(0).getVal());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("GeoTargetingEditPage", one.getName());
+    }
+
 }
