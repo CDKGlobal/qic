800101b07f765dbfae0a4bd237be5014e7748698
Christopher Chen <chenc@cobalt.com>
2014-07-02 16:44:15 +0000

    added new test

--- src/test/java/com/testing123/vaadin/DataPointTest.java
+++ src/test/java/com/testing123/vaadin/DataPointTest.java
@@ -10,10 +10,43 @@ public class DataPointTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private DataPoint dataPointFactory() {
+		return new DataPoint();
 	}
 
+	@Test
+	public void TestEmptyDataPointReturnsDefaultValues() {
+		DataPoint data = dataPointFactory();
+		assertEquals(null, data.getKey());
+		assertEquals(-1, (int)data.getComplexity());
+		assertEquals(-1, (int)data.getLineOfCode());
+	}
+	
+	@Test
+	public void TestSetAndGetKey() {
+		DataPoint data = dataPointFactory();
+		data.setKey("key");
+		assertEquals("key", data.getKey());
+		assertEquals(-1, (int)data.getComplexity());
+		assertEquals(-1, (int)data.getLineOfCode());
+	}
+
+	@Test
+	public void TestSetAndGetComplexity() {
+		DataPoint data = dataPointFactory();
+		data.setComplexity(100);
+		assertEquals(null, data.getKey());
+		assertEquals(100, (int)data.getComplexity());
+		assertEquals(-1, (int)data.getLineOfCode());
+	}
+	
+	@Test
+	public void TestSetAndGetLineOfCode() {
+		DataPoint data = dataPointFactory();
+		data.setLineOfCode(500);
+		assertEquals(null, data.getKey());
+		assertEquals(-1, (int)data.getComplexity());
+		assertEquals(500, (int)data.getLineOfCode());
+	}
 }
