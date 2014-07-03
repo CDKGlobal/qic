0a187e2ac3109943dedd9996dd512a539c34c1cc
Weiyou Dai <weiyoud@cobalt.com>
2014-07-03 20:03:33 +0000

    Changed FrmtVal name

--- src/main/java/com/testing123/vaadin/Msr.java
+++ src/main/java/com/testing123/vaadin/Msr.java
@@ -1,31 +1,31 @@
 package com.testing123.vaadin;
 
 public class Msr {
-	private String key;
-	private double val;
-	private String frmt_val;
+    private String key;
+    private double val;
+    private String frmtVal;
 
-	public String getKey() {
-		return key;
-	}
+    public String getKey() {
+        return key;
+    }
 
-	public double getVal() {
-		return val;
-	}
+    public double getVal() {
+        return val;
+    }
 
-	public String getFrmt_val() {
-		return frmt_val;
-	}
+    public String getFrmtVal() {
+        return frmtVal;
+    }
 
-	public void setKey(String key) {
-		this.key = key;
-	}
+    public void setKey(String key) {
+        this.key = key;
+    }
 
-	public void setVal(double val) {
-		this.val = val;
-	}
+    public void setVal(double val) {
+        this.val = val;
+    }
 
-	public void setFrmt_val(String frmt_val) {
-		this.frmt_val = frmt_val;
-	}
+    public void setFrmVal(String frmtVal) {
+        this.frmtVal = frmtVal;
+    }
 }
