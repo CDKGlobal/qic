0300c09c73f73dd547197e404b50793c68148a34
Christopher Chen <chenc@cobalt.com>
2014-07-08 00:09:12 +0000

    downloader (2)

---
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -0,0 +1,9 @@
+package com.testing123.downloader;
+
+public class DataDownloaderMain {
+	
+	public static void main(String[] args) {
+		Downloader dl = new Downloader();
+		dl.downloadProjects("platform");
+	}
+}
---
+++ src/main/java/com/testing123/downloader/Downloader.java
@@ -0,0 +1,97 @@
+package com.testing123.downloader;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.testing123.vaadin.DataPoint;
+import com.testing123.vaadin.Reader2;
+import com.testing123.vaadin.WebData;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Downloader {
+	private JsonFactory jsonFactory;
+	private ObjectMapper mapper;
+
+	public Downloader() {
+		this.jsonFactory = new JsonFactory();
+		this.mapper = new ObjectMapper();
+	}
+	
+	public void downloadProjects(String projectName) {
+		String projectLink = "http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:" + projectName + "&depth=0&metrics=ncloc,complexity&format=json";
+		try {
+			URL projectURL = new URL(projectLink);
+			List<WebData> projectList = mapper.readValue(projectURL, new TypeReference<List<WebData>>() {});
+			
+			// makes a folder with today's date on it
+			String currentPath = makeFolder("", projectList.get(0).getDate());
+			
+			// creates projects
+			writeJson(currentPath, projectList, projectList.get(0).getId() + "");
+			currentPath = makeFolder(currentPath, projectList.get(0).getId() + "");
+			
+			System.out.println("WRITING FOLDERS");
+			// creates directories
+			URL folderURL = new URL("http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:" + projectName + "&depth=-1&scopes=DIR&format=json");
+			List<WebData> folderList = mapper.readValue(folderURL, new TypeReference<List<WebData>>() {});
+			writeJson(currentPath, folderList, "folders");
+			
+			System.out.println("WRITING FILES");
+			List<WebData> fileList = new ArrayList<WebData>();
+			for (WebData folder : folderList) {
+				String currentFolder = "http://sonar.cobalt.com/api/resources?resource="
+						+ folder.getKey()
+						+ "&depth=1&" 
+						+ "metrics=ncloc,complexity&format=json";
+				URL filesLink = new URL(currentFolder);
+				List<WebData> currentList = mapper.readValue(filesLink, new TypeReference<List<WebData>>() {});
+				for (WebData b : currentList) {
+					fileList.add(b);
+				}
+				System.out.println("bang!");
+			}
+			writeJson(currentPath, fileList, "files");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void writeJson(String path, List<WebData> data, String name) {
+		String filePath = path + name;
+		try {
+			for (WebData file : data) {
+				mapper.writeValue(new File(filePath + ".json"), data);
+				System.out.println("writing file: " + file.getName());
+			}
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String makeFolder(String path, String name) {
+		File folder = new File(path + name);
+		if (!folder.exists()) {
+			System.out.println("creating directory: " + path + name);
+			try {
+				folder.mkdir();
+			} catch (SecurityException se) {
+				se.printStackTrace();
+			}
+		}
+		return path + name + "/";
+	}
+}
