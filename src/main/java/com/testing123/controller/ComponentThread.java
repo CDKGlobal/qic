8a3a7c27abdd7c4413bd1c3ad9520b471a71dee7
Christopher Chen <chenc@cobalt.com>
2014-07-09 23:46:50 +0000

    fixed import errors

---
+++ src/main/java/com/testing123/controller/ComponentThread.java
@@ -0,0 +1,22 @@
+package com.testing123.controller;
+
+public class ComponentThread extends Thread {
+//
+//	@Override
+//	public void run() {
+//		try {
+//			access(new Runnable() {
+//				@Override
+//				public void run() {
+//
+//					// Add the data point, and drop the
+//					// first one if there's already
+//					// more than 10 points.
+//					series.add(new DataSeriesItem(x++, y), true, x > 10);
+//				}
+//			});
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+}
