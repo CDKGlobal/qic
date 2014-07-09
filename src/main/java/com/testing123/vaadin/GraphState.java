28a1af9d96ec9ab877dbeab36e122f7bb18205ee
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-09 21:53:43 +0000

    formatted?

--- src/main/java/com/testing123/vaadin/GraphState.java
+++ src/main/java/com/testing123/vaadin/GraphState.java
@@ -11,45 +11,47 @@ import com.vaadin.shared.ui.JavaScriptComponentState;
  * @author chenc
  */
 @SuppressWarnings("serial")
-public class GraphState extends JavaScriptComponentState{
+public class GraphState extends JavaScriptComponentState {
 	/** the JSONArray of data points to be plotted **/
 	private JSONArray data;
 	/** the JSONObject of settings for the graph **/
 	private JSONObject options;
 
-    /**
-     * Sets the options of for the graph.
-     * 
-     * @param data
-     */
-    public void setData(JSONArray data) {
-        this.data = data;
-    }
-	
 	/**
 	 * Sets the options of for the graph.
 	 * 
-	 * @param options the JSONObject that has a format that the JavaScript flot library can read
+	 * @param data
+	 */
+	public void setData(JSONArray data) {
+		this.data = data;
+	}
+
+	/**
+	 * Sets the options of for the graph.
+	 * 
+	 * @param options
+	 *            the JSONObject that has a format that the JavaScript flot
+	 *            library can read
 	 */
 	public void setOptions(JSONObject options) {
 		this.options = options;
 	}
-	
+
 	/**
 	 * Returns the current data stored for the graph
 	 * 
 	 * @return data
 	 */
-    public JSONArray getData() {
-        return data;
-    }
-    
-    /**
-     * Returns the current options stored for the graph
-     * 
-     * @return options
-     */
-    public JSONObject getOptions() {
-    	return options;
-    }
+	public JSONArray getData() {
+		return data;
+	}
+
+	/**
+	 * Returns the current options stored for the graph
+	 * 
+	 * @return options
+	 */
+	public JSONObject getOptions() {
+		return options;
+	}
 }
