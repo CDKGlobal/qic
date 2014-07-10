252ede109fb6768858a363a968e25e916aacfc50
Christopher Chen <chenc@cobalt.com>
2014-07-10 19:55:34 +0000

    calendar class

--- src/main/java/com/testing123/controller/AvailableResources.java
+++ src/main/java/com/testing123/controller/AvailableResources.java
@@ -2,9 +2,23 @@ package com.testing123.controller;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
 
 public class AvailableResources {
+	
+	public static Calendar convertToDate(String sonarFormat) {
+		String[] dateAndTime = sonarFormat.split("T");
+        String[] date = dateAndTime[0].split("-");
+        String[] time = dateAndTime[1].split("-");
+		Calendar currentDate = new GregorianCalendar(
+				Integer.parseInt(date[0]), Integer.parseInt(date[1]) - 1, Integer.parseInt(date[2]),
+				Integer.parseInt(time[0]), Integer.parseInt(time[1]), Integer.parseInt(time[2]));
+		currentDate.setTimeZone(TimeZone.getTimeZone("GMT-" + time[3]));
+		return currentDate;
+	}
 	
 	public static List<String> getAvailableDates() {
 		List<String> dates = new ArrayList<String>();
@@ -14,13 +28,12 @@ public class AvailableResources {
 				+ "/Perforce/chenc_sea-chenc-m_qic/Playpen/QIC2/Archives/";
 			File folder = new File(absolutePath);
 			String[] names = folder.list();
+			List<Calendar> availDates = new ArrayList<Calendar>();
 			for(String name : names) {
 			    if (new File(absolutePath + name).isDirectory()) {
-			    	String[] dateAndTime = name.split("T");
-			        String[] date = dateAndTime[0].split("-");
-			        String[] time = dateAndTime[1].split("-");
-			        dates.add(name);
-			    	System.out.println(name);
+			    	Calendar convertedDate = convertToDate(name);
+			        availDates.add(convertedDate);
+			    	dates.add(convertedDate.getTime().toString());
 			    }
 			}
 		} catch (Exception e) {
--- src/main/java/com/testing123/controller/ComponentController.java
+++ src/main/java/com/testing123/controller/ComponentController.java
@@ -1,16 +1,18 @@
 package com.testing123.controller;
 
+import java.util.Calendar;
+
 import com.testing123.ui.MainComponent;
 import com.vaadin.ui.GridLayout;
 
 public class ComponentController {
 	
-	public static void drawComponent(GridLayout layout) {
+	public static void drawMainComponent(GridLayout layout, Calendar start, Calendar end) {
 		MainComponent comp = new MainComponent();
 		layout.addComponent(comp, 1, 1);
 	}
 	
-	public static void drawGraph() {
+	public static void drawLabels() {
 		
 	}
 }
