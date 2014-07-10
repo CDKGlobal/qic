6ed7a9c24cd7828447c5b9b7b8db26d02a4e35ac
Christopher Chen <chenc@cobalt.com>
2014-07-10 16:33:20 +0000

    updated qicui

--- src/main/java/com/testing123/controller/ComponentController.java
+++ src/main/java/com/testing123/controller/ComponentController.java
@@ -1,12 +1,12 @@
 package com.testing123.controller;
 
-import com.testing123.ui.MainComponentVolatile;
+import com.testing123.ui.MainComponent;
 import com.vaadin.ui.GridLayout;
 
 public class ComponentController {
 	
 	public static void drawComponent(GridLayout layout) {
-		MainComponentVolatile comp = new MainComponentVolatile();
+		MainComponent comp = new MainComponent();
 		layout.addComponent(comp, 1, 1);
 	}
 	
--- src/main/java/com/testing123/ui/QicUI.java
+++ src/main/java/com/testing123/ui/QicUI.java
@@ -66,29 +66,7 @@ public class QicUI extends UI {
 		//new ComponentThread().start();
 		ComponentController.drawComponent(layout);
 		
-		NavComponentVolatile nav = new NavComponentVolatile(layout);
+		NavigationComponent nav = new NavigationComponent(layout);
 		layout.addComponent(nav, 1, 2);
 	}
-	
-//	class ComponentThread extends Thread {
-//
-//		@Override
-//		public void run() {
-//	        try {
-//				access(new Runnable() {
-//					
-//					@Override
-//					public void run() {
-//
-//						// Add the data point, and drop the
-//						// first one if there's already
-//						// more than 10 points.
-//						series.add(new DataSeriesItem(x++, y), true, x > 10);
-//					}
-//				});
-//	        } catch (InterruptedException e) {
-//	            e.printStackTrace();
-//	        }
-//	    }
-//	}
 }
