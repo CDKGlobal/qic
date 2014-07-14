55e752ac620730b0dcb1099759c11268a4a38371
Weiyou Dai <weiyoud@cobalt.com>
2014-07-14 21:08:19 +0000

    Main program to run the DownloaderAndToTxt

---
+++ src/main/java/com/testing123/vaadin/DownloaderAndToTxtMain.java
@@ -0,0 +1,11 @@
+package com.testing123.vaadin;
+
+public class DownloaderAndToTxtMain {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DownloadAndToTxt downloader = new DownloadAndToTxt();
+        downloader.downloadProjects("platform");
+    }
+
+}
