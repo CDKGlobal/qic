b1ffc1c175158bedadd7dcd0d0dd1d6c7d9add5e
Weiyou Dai <weiyoud@cobalt.com>
2014-07-14 21:01:56 +0000

    Combination of Downloader and JsonToTxt files

---
+++ src/main/java/com/testing123/vaadin/DownloadAndToTxt.java
@@ -0,0 +1,126 @@
+package com.testing123.vaadin;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Downloader downloads all the metric data available from Sonar at the time of execution.  To use,
+ * make an instance of this class, and call downloadProject().
+ * 
+ * @author chenc
+ */
+public class DownloadAndToTxt {
+    private final ObjectMapper mapper;
+
+    public DownloadAndToTxt() {
+        mapper = new ObjectMapper();
+    }
+
+    /**
+     * Downloads all the metric data (including package/directory/file data) available
+     * 
+     * @param projectName the project name
+     */
+    public void downloadProjects(String projectName) {
+        String projectLink = "http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:" + projectName + "&depth=0&metrics=ncloc,complexity&format=json";
+        try {
+            URL projectURL = new URL(projectLink);
+            List<WebData> projectList = mapper.readValue(projectURL, new TypeReference<List<WebData>>() {});
+
+            // makes a folder with today's date on it
+            String currentPath = makeFolder("Archives/", projectList.get(0).getDate().replace(":", "-"));
+
+
+            // creates projects
+            writeJson(currentPath, projectList, projectList.get(0).getId() + "");
+            currentPath = makeFolder(currentPath, projectList.get(0).getId() + "");
+
+            System.out.println("WRITING FOLDERS");
+            // creates directories
+            URL folderURL = new URL("http://sonar.cobalt.com/api/resources?resource=com.cobalt.dap:" + projectName + "&depth=-1&metrics=ncloc,complexity&scopes=DIR&format=json");
+            List<WebData> folderList = mapper.readValue(folderURL, new TypeReference<List<WebData>>() {});
+            writeJson(currentPath, folderList, "folders");
+
+            System.out.println("WRITING FILES");
+            List<WebData> fileList = new ArrayList<WebData>();
+            for (WebData folder : folderList) {
+                String currentFolder = "http://sonar.cobalt.com/api/resources?resource="
+                                + folder.getKey()
+                                + "&depth=1&"
+                                + "metrics=ncloc,complexity&format=json";
+                URL filesLink = new URL(currentFolder);
+                List<WebData> currentList = mapper.readValue(filesLink, new TypeReference<List<WebData>>() {});
+                for (WebData b : currentList) {
+                    fileList.add(b);
+                }
+                System.out.println("bang!");
+            }
+            writeTxt(currentPath, fileList, "files");
+            writeJson(currentPath, fileList, "files");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeJson(String path, List<WebData> data, String name) {
+        String filePath = path + name;
+        try {
+            int i = 0;
+            for (WebData file : data) {
+                mapper.writeValue(new File(filePath + ".json"), data);
+                if ("CLA".equals(file.getQualifier())) {
+                    i++;
+                    System.out.println("writing file: " + file.getKey());
+                }
+            }
+            System.out.println(i);
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeTxt(String path, List<WebData> data, String name) {
+        String filePath = path + name;
+        try {
+            PrintWriter writer = new PrintWriter(filePath + ".txt");
+            for (WebData file : data) {
+                writer.print(file.getId() + "\t" + file.getKey() + "\t" + file.getName() + "\t" + file.getScope()
+                                + "\t" + file.getQualifier() + "\t" + file.getDate());
+                if (file.getMsr() == null) {
+                    writer.println("\t-1.0 \t -1.0");
+                } else {
+                    writer.println("\t" + file.getMsr().get(0).getVal() + "\t" + file.getMsr().get(1).getVal());
+                }
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String makeFolder(String path, String name) {
+        File folder = new File(path + name + "(1)");
+        if (!folder.exists()) {
+            System.out.println("creating directory: " + folder.getAbsolutePath());
+            try {
+                folder.mkdir();
+            } catch (SecurityException se) {
+                se.printStackTrace();
+            }
+        }
+        return path + name + "/";
+    }
+}
