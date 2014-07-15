80e56ac1f6becbe2195c28e91fbbfcf33b563626
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-15 16:02:38 +0000

    refactored and added exception catching

--- src/main/java/com/testing123/vaadin/FisheyeChurn.java
+++ src/main/java/com/testing123/vaadin/FisheyeChurn.java
@@ -1,60 +1,75 @@
 package com.testing123.vaadin;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FisheyeChurn {
 
-	public static Set<DataPoint> getData(){
-		
+	public static Set<DataPoint> getData() {
+
 		Set<DataPoint> dataSet = new HashSet<DataPoint>();
-		
-		FisheyeData querriedData = FisheyeChurnGetJson();
-		
+
+		FisheyeData querriedData = fisheyeChurnGetJson();
+
 		for (ItemData i : querriedData.getRow()) {
-			if((Integer)i.getItem(3)==0){
-				String pathName = (String)i.getItem(0);
+			if ((Integer) i.getItem(3) == 0) {
+				String pathName = (String) i.getItem(0);
 				String[] split = pathName.split("/");
-			    String name=split[split.length-1].replace(".java", "");
-				
-				dataSet.add(new DataPoint(name,(Integer)i.getItem(1),(Integer)i.getItem(2)));
+				String name = split[split.length - 1].replace(".java", "");
+
+				dataSet.add(new DataPoint(name, (Integer) i.getItem(1), (Integer) i.getItem(2)));
 			}
 		}
-		System.out.println();System.out.println(dataSet);
+		System.out.println();
+		System.out.println(dataSet);
 		return dataSet;
 	}
 
-	public static FisheyeData FisheyeChurnGetJson() {
-		return FisheyeChurnGetJson("2013-07-07", "2014-07-08");
+	public static FisheyeData fisheyeChurnGetJson() {
+		return fisheyeChurnGetJson("2013-07-07", "2014-07-08");
 	}
-	
 
-	public static FisheyeData FisheyeChurnGetJson(String startDate, String endDate){
-		
+	public static FisheyeData fisheyeChurnGetJson(String startDate, String endDate) {
+
 		ObjectMapper mapper = new ObjectMapper();
 
 		String query = getQuery(startDate, endDate, "Platform");
 		String link = getLink("Advertising", query);
 		FisheyeData querriedData;
-		
+
+		URL projectURL;
+		querriedData = new FisheyeData();
 		try {
-			URL projectURL = new URL(link);
-			System.out.println(projectURL);
+			projectURL = new URL(link);
 			querriedData = mapper.readValue(projectURL, new TypeReference<FisheyeData>() {
 			});
-			return querriedData;
-		} catch (Exception e) {
-			return new FisheyeData();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return querriedData;
 
 	}
-	
-	public static String getLink(){
-		String query = getQuery("2014-07-07", "2014-07-08","Platform");
+
+	public static String getLink() {
+		String query = getQuery("2014-07-07", "2014-07-08", "Platform");
 		return getLink("Advertising", query);
 	}
 
@@ -66,37 +81,11 @@ public class FisheyeChurn {
 
 		return "select%20revisions%20" + dateRange + "%20" + directory + "%20" + java + "%20group%20by%20file%20return%20" + returned;
 	}
-	
-	private static String getLink(String directory, String query){
+
+	private static String getLink(String directory, String query) {
 		String home = "http://fisheye.cobalt.com/rest-service-fe/search-v1/queryAsRows";
 		String directoryPath = "/Advertising.Perforce" + ".json";
 		return home + directoryPath + "?query=" + query + "&maxReturn=3000";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
