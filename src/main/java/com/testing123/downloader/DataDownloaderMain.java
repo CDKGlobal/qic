f46b50190aeeb2330b692809c31fa77e1a033b75
Weiyou Dai <weiyoud@cobalt.com>
2014-07-15 22:44:09 +0000

    Added trigger for DatabaseConnector

--- src/main/java/com/testing123/downloader/DataDownloaderMain.java
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -1,10 +1,13 @@
 package com.testing123.downloader;
 
 public class DataDownloaderMain {
-	
-	public static void main(String[] args) {
-		Downloader dl = new Downloader();
-		// downloads everything from the platform project
-		dl.downloadProjects("platform");
-	}
+
+    public static void main(String[] args) {
+        Downloader dl = new Downloader();
+        // downloads everything from the platform project
+        dl.downloadProjects("platform");
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        String date = Downloader.today;
+        dbConnector.connectAndExecute(date);
+    }
 }
