7b384c452f00296f615c6550293ce69840bb5788
Weiyou Dai <weiyoud@cobalt.com>
2014-07-16 17:42:48 +0000

    Added junit test

---
+++ src/main/java/com/testing123/downloader/DatabaseConnectorTest.java
@@ -0,0 +1,24 @@
+package com.testing123.downloader;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DatabaseConnectorTest {
+    private DatabaseConnector dbConnector;
+
+    @BeforeClass
+    public void setUpBeforeClass() throws Exception {
+        dbConnector = new DatabaseConnector();
+    }
+
+    @Test
+    public void testGetConnection() {
+
+    }
+
+    @Test
+    public void testCreateDbAndLoadTable() {
+
+    }
+
+}
