4fd1670de6788c00c61e66903dfe0374b3eb4271
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-16 20:17:37 +0000

    an updated FisheyeChurn

---
+++ src/main/java/com/testing123/vaadin/RetrieveFisheyeData.java
@@ -0,0 +1,66 @@
+package com.testing123.vaadin;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RetrieveFisheyeData {
+
+	public Map<String,Integer> getChurnData(ConvertDate startDate, ConvertDate endDate) {
+		Map<String,Integer> churnData = new TreeMap<String,Integer>();
+		FisheyeData querriedData = getJSONFromFisheye(startDate, endDate);
+		int pathIndex = querriedData.getHeadings().indexOf("path");
+		int sumLinesAddedIndex = querriedData.getHeadings().indexOf("sumLinesAdded");
+		int sumLinesRemovedIndex = querriedData.getHeadings().indexOf("sumLinesRemoved");
+		int isDeletedIndex = querriedData.getHeadings().indexOf("countIsDeleted");
+		
+		for (ItemData i : querriedData.getRow()) {
+			if (isNotDeleted(i, isDeletedIndex)) {
+				String path = formatPath(i,pathIndex);
+				int churn = (Integer) i.getItem(sumLinesAddedIndex) + (Integer) i.getItem(sumLinesRemovedIndex);
+				churnData.put(path, churn);
+			}
+		}
+		System.out.println(churnData.entrySet().toString());
+		return churnData;
+	}
+	
+	private boolean isNotDeleted(ItemData i, int isDeletedIndex){
+		return (Integer) i.getItem(isDeletedIndex) == 0 ;
+	}
+	
+	private String formatPath(ItemData i, int pathIndex){
+		String path = (String) i.getItem(pathIndex);
+		String[] split = path.split("/");
+		String pathName = split[split.length - 1].replace(".java", "");
+		return pathName;
+	}
+
+	public static FisheyeData getJSONFromFisheye(ConvertDate startDate, ConvertDate endDate) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		URL url = new FisheyeQuery("Advertising.Perforce", startDate, endDate).getChurn();
+		FisheyeData querriedData = new FisheyeData();
+		try {
+			querriedData = mapper.readValue(url, new TypeReference<FisheyeData>() {
+			});
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return querriedData;
+	}
+
+}
