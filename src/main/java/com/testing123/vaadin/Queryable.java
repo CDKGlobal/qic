7f6a7f5004899ff1e32d7dbf1aa061673c9a02d1
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-17 16:33:58 +0000

    Query the database using Queryable interface

--- src/main/java/com/testing123/vaadin/Queryable.java
+++ src/main/java/com/testing123/vaadin/Queryable.java
@@ -4,6 +4,6 @@ import java.util.Map;
 
 public interface Queryable {
 	
-	public Map<String, Double> getInstantaneousValue(ConvertDate date, String metric);
+	public Map<String, Double> getInstantaneousValue(ConvertDate date, String qualifier, String scope, String path, String metric);
 
 }
