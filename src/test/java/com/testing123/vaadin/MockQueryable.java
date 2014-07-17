f0e5a131bbdf22a26fb36a5a9569b7dd2757a643
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-17 22:10:04 +0000

    Mocks a queryable interface

---
+++ src/test/java/com/testing123/vaadin/MockQueryable.java
@@ -0,0 +1,29 @@
+package com.testing123.vaadin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockQueryable implements Queryable {
+	private Map<String, Double>churn;
+	private Map<String,Double> complexity;
+
+	@Override
+	public Map<String, Double> getChurn(ConvertDate startDate, ConvertDate endDate) {
+		return churn;
+	}
+
+	@Override
+	public Map<String, Double> getComplexity(ConvertDate date) {
+		return complexity;
+	}
+	
+	public MockQueryable(){
+		churn = new HashMap<String, Double>();
+		complexity = new HashMap<String, Double>();
+		churn.put("a", 13.0);
+		churn.put("b", 28.0);
+		complexity.put("b", 11.0);
+		complexity.put("c", 3333333.33);
+	}
+
+}
