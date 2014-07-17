bfa7b15ccd84fc64a99f5b53c8594462ab845c88
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-17 21:46:40 +0000

    chenged interface

--- src/main/java/com/testing123/vaadin/Queryable.java
+++ src/main/java/com/testing123/vaadin/Queryable.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public interface Queryable {
 	
-	public Map<String, Double> getInstantaneousValue(ConvertDate date, String qualifier, String scope, String path, String metric);
+	public Map<String, Double> getChurn(ConvertDate startDate, ConvertDate endDate);
+	
+	public Map<String, Double> getComplexity(ConvertDate date);
 
 }
