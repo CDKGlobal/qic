7fb2dd088fcadaa55c3482f3ded93327384f3b8d
Weiyou Dai <weiyoud@cobalt.com>
2014-07-21 21:42:53 +0000

    Divided the methods of databaseConnector into single functions

--- src/main/java/com/testing123/downloader/DataDownloaderMain.java
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -1,13 +1,21 @@
 package com.testing123.downloader;
 
-public class DataDownloaderMain {
+import java.sql.Connection;
 
+public class DataDownloaderMain {
     public static void main(String[] args) {
-        Downloader dl = new Downloader();
-        // downloads everything from the platform project
-        dl.downloadProjects("platform");
-        DatabaseConnector dbConnector = new DatabaseConnector();
-        String date = Downloader.today;
-        dbConnector.connectAndExecute(date);
+        try {
+            Downloader dl = new Downloader();
+            // downloads everything from the platform project
+            dl.downloadProjects("platform");
+            DatabaseConnector dbConnector = new DatabaseConnector();
+            String todayDate = Downloader.today;
+            // builds a connection
+            Connection conn = dbConnector.getConnection();
+            dbConnector.createDbAndLoadTable(conn, todayDate);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
