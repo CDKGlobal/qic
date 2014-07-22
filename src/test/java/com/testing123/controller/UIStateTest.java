4d219472c5c5f8b9cc35f09d02bf2834b779ae27
Christopher Chen <chenc@cobalt.com>
2014-07-22 16:23:14 +0000

    updated better tests

--- src/test/java/com/testing123/controller/UIStateTest.java
+++ src/test/java/com/testing123/controller/UIStateTest.java
@@ -6,27 +6,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.testing123.controller.UIState;
+import com.testing123.controller.UIState.Axis;
+import com.testing123.vaadin.ConvertDate;
 
 public class UIStateTest {
-	private static UIState state;
-
-	@Before
-	public void setUp() throws Exception {
-		state = new UIState();
-	}
 
 	@Test
 	public void TestInitialUIState() {
+		UIState state = new UIState();
 		assertEquals("Tue Jul 15 06:07:55 PDT 2014", state.getStart().toString());
 		assertEquals("Mon Jul 21 06:07:35 PDT 2014", state.getEnd().toString());
-		//assertEquals(true, state.getIsDelta());
+		assertEquals(Axis.DELTA_LINESOFCODE, state.getX());
+	}
+	
+	@Test
+	public void TestInitialUIStateWithArgs() {
+		UIState state = new UIState(new ConvertDate("2014-07-15T06-07-55-0700"), 
+				new ConvertDate("2014-07-21T06-07-35-0700"), Axis.COMPLEXITY);
+		assertEquals("Tue Jul 15 06:07:55 PDT 2014", state.getStart().toString());
+		assertEquals("Mon Jul 21 06:07:35 PDT 2014", state.getEnd().toString());
+		assertEquals(Axis.COMPLEXITY, state.getX());
 	}
 	
 	@Test
 	public void TestSetUIState() {
-		//state.setIsDelta(false);
-		//assertEquals(false, state.getIsDelta());
-		state.verifyState();
+		UIState state = new UIState();
+		state.setFocus("string");
+		assertEquals("string", state.getFocus());
 	}
 
 }
