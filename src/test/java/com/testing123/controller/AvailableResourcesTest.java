58a2b045ffb4ba475e330434c534bef72ea3d810
Christopher Chen <chenc@cobalt.com>
2014-07-22 15:40:17 +0000

    potential working test added

--- src/test/java/com/testing123/controller/AvailableResourcesTest.java
+++ src/test/java/com/testing123/controller/AvailableResourcesTest.java
@@ -11,19 +11,19 @@ import com.testing123.vaadin.WebData;
 
 public class AvailableResourcesTest {
 
-//	@Test
-//	public void TestGetAvailableDatesReturnsDates() {
-////		assertEquals("2014-07-07T06-09-17-0700", AvailableResources.getAvailableDates().get(0));
-//	}
+	@Test
+	public void TestGetAvailableDatesReturnsDates() {
+//		assertEquals("2014-07-07T06-09-17-0700", AvailableResources.getAvailableDates().get(0));
+	}
 	
 	@Test
 	public void TestSQLConnectionThrowsNoException() {
-        //SQLConnector.dataQuery("", "");
+        SQLConnector.dataQuery("", "");
 	}
 	
 	@Test 
 	public void TestInvalidGetDataListReturnsNonEmptyList() {
-		//List<WebData> list = AvailableResources.getDataList("d07_07");
-		//assertTrue(list.size() > 0);
+//		List<WebData> list = AvailableResources.getDataList("2014-07-07T06-09-17-0700");
+//		assertTrue(list.size() > 0);
 	}
 }
