35905471bb67bd66707ad5d24131cacd57cd5491
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-23 20:17:19 +0000

    added tests the use the comparable feature of DataPoint

--- src/test/java/com/testing123/vaadin/GetDataTest.java
+++ src/test/java/com/testing123/vaadin/GetDataTest.java
@@ -2,6 +2,10 @@ package com.testing123.vaadin;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,14 +17,14 @@ public class GetDataTest {
 	static Queryable qi;
 	GetData gd;
 	UIState state;
-	
+
 	@BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        qi = new MockQueryable();
-    }
-	
+	public static void setUpBeforeClass() throws Exception {
+		qi = new MockQueryable();
+	}
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		gd = new GetData(qi);
 		state = new UIState();
 	}
@@ -28,22 +32,31 @@ public class GetDataTest {
 	@Test
 	public void passInDLOCGetDLOCvsComplexity() {
 		state.setX(Axis.DELTA_LINESOFCODE);
-		System.out.println();
-		assertEquals(3,gd.getData(state).size());
+		Set<DataPoint> set = gd.getData(state);
+		assertEquals(3, set.size());
+		assertEquals("[[28.0, 11.0, \"b\", \"Authors: \"], [32.0, 13.0, \"c\", \"Authors: \"], [8.0, 14.0, \"t\", \"Authors: \"]]",
+				HashSetToTreeSet(set).toString());
 	}
-	
+
 	@Test
 	public void passInLOCgetLOCvsComplexity() {
 		state.setX(Axis.LINESOFCODE);
-		System.out.println();
-		assertEquals(5,gd.getData(state).size());
+		Set<DataPoint> set = gd.getData(state);
+		assertEquals(5, set.size());
+		assertEquals(
+"[[13.0, 11.0, \"b\", \"Authors: \"], [15.0, 11.0, \"d\", \"Authors: \"], [8.0, 11.0, \"i\", \"Authors: \"], [11.0, 54.0, \"p\", \"Authors: \"], [6.0, 14.0, \"t\", \"Authors: \"]]",
+HashSetToTreeSet(set).toString());
+
 	}
-	
+
 	@Test
 	public void passInEmptyDComplexReturnEmpty() {
 		state.setX(Axis.DELTA_COMPLEXITY);
-		System.out.println();
-		assertEquals("[]",gd.getData(state).toString());
+		assertEquals("[]", gd.getData(state).toString());
+	}
+
+	public TreeSet<DataPoint> HashSetToTreeSet(Set<DataPoint> set) {
+		return new TreeSet<DataPoint>(set);
 	}
 
 }
