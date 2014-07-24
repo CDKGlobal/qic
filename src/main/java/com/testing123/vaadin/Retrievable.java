86d1c1dac07f943cbb805b0753b9d6d35c53cf29
Christopher Chen <chenc@cobalt.com>
2014-07-24 22:26:41 +0000

    updated interface for filters

--- src/main/java/com/testing123/vaadin/Retrievable.java
+++ src/main/java/com/testing123/vaadin/Retrievable.java
@@ -1,5 +1,6 @@
 package com.testing123.vaadin;
 
+import java.util.List;
 import java.util.Set;
 
 import com.testing123.controller.UIState;
@@ -12,7 +13,7 @@ public interface Retrievable {
 	 * @param state the current data and state required for the UI
 	 * @return a set of DataPoints that represents the data required to plot the graph
 	 */
-	public Set<DataPoint> getData(UIState state);
+	public Set<DataPoint> getData(UIState state, List<String> projects, List<String> authors);
 	
 	
 }
