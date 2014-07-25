b162633f071fd139e72cc3df3a461c7ce9ab7a7e
Christopher Chen <chenc@cobalt.com>
2014-07-25 16:26:05 +0000

    supports projects

---
+++ src/main/java/com/testing123/vaadin/ConvertProject.java
@@ -0,0 +1,24 @@
+package com.testing123.vaadin;
+
+public class ConvertProject {
+	private String name;
+	private String key;
+
+	public ConvertProject(String name, String key) {
+		this.name = name;
+		this.key = key;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getKey() {
+		return key;
+	}
+}
