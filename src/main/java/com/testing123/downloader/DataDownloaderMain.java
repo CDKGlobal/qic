5625987a4e871fe3ee7ff93a755b4b62b18c223f
Weiyou Dai <weiyoud@cobalt.com>
2014-07-25 20:29:29 +0000

    add a method to get the previous file's idList

--- src/main/java/com/testing123/downloader/DataDownloaderMain.java
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -1,21 +1,39 @@
 package com.testing123.downloader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DataDownloaderMain {
     public static void main(String[] args) {
         try {
             Downloader dl = new Downloader();
             // downloads everything from the platform project
-            dl.downloadProjects("platform");
+            ArrayList<Integer> idList = formerIdList();
+            dl.downloadProjects("platform", idList);
             DatabaseConnector dbConnector = new DatabaseConnector();
             String todayDate = Downloader.today;
             // builds a connection
             Connection conn = dbConnector.getConnection();
-            dbConnector.createDbAndLoadTable(conn, todayDate);
+            dbConnector.createDbAndLoadTable(conn, todayDate, "platform");
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<Integer> formerIdList() throws FileNotFoundException {
+        ArrayList<Integer> idList = new ArrayList<Integer>();
+        Scanner input = new Scanner(new File("src/main/java/com/testing123/downloader/files0723.txt"));
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            int id = lineScanner.nextInt();
+            idList.add(id);
+        }
+        return idList;
+
     }
 }
