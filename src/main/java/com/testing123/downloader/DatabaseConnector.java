f081890d3aa557fcf02a85ba9743f5091d1e56c0
Weiyou Dai <weiyoud@cobalt.com>
2014-07-28 21:26:38 +0000

    Added new lines to construct new databases

--- src/main/java/com/testing123/downloader/DatabaseConnector.java
+++ src/main/java/com/testing123/downloader/DatabaseConnector.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
+
+import com.testing123.vaadin.WebData;
 
 public class DatabaseConnector {
     /*   public void connectAndExecute(String date) {
@@ -23,7 +26,7 @@ public class DatabaseConnector {
         try {
             // Properties connectionProps = new Properties();
             Connection conn =
-                            DriverManager.getConnection("jdbc:mysql://localhost/dataList?" +
+                            DriverManager.getConnection("jdbc:mysql://localhost/dataList2?" +
                                             "user=root&password=password");
             // Do something with the Connection
             return conn;
@@ -36,24 +39,47 @@ public class DatabaseConnector {
         return null;
     }
 
+    public void createDbAndLoadTableForPrject(Connection conn, List<WebData> projectList) {
+        Statement stmt = null;
+        boolean rs = true;
+        System.out.println("Ok before conn.createStatement");
+        try {
+            stmt = conn.createStatement();
+            for (WebData project : projectList) {
+                System.out.println(project.getId() + "_id");
+                rs = stmt.execute("CREATE TABLE id_" + project.getId() + " (id INT NOT NULL, "
+                                + "the_key VARCHAR(160) NOT NULL, name VARCHAR(80) NOT NULL, "
+                                + "scope VARCHAR(5) NOT NULL, qualifier VARCHAR(5) NOT NULL," +
+                                "date VARCHAR(30) NOT NULL,"
+                                + " ncloc DECIMAL(8,1), complexity DECIMAL(8,1));");
+                rs = stmt.execute("LOAD DATA LOCAL INFILE '//Users/weiyoud/Perforce/weiyoud_sea-weiyoud_4033/Playpen/QIC2/Archives/"
+                                + "projectList/" + project.getId() + ".txt' INTO TABLE id_" + project.getId() + ";");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void createDbAndLoadTable(Connection conn, String date, String projectName) {
         Statement stmt = null;
         boolean rs = true;
         try {
             stmt = conn.createStatement();
-            /*
-             * rs = stmt.execute("CREATE TABLE " + date.replace("-", "_") + "(id INT NOT NULL, "
-             * + "the_key VARCHAR(160) NOT NULL, name VARCHAR(80) NOT NULL, "
-             * + "scope VARCHAR(5) NOT NULL, qualifier VARCHAR(5) NOT NULL,"
-             * + " date VARCHAR(30) NOT NULL,"
-             * + " ncloc DECIMAL(5,1), complexity DECIMAL(4,1));");
-             */
+            //       for (WebData project : projectList) {
+
+            rs = stmt.execute("CREATE TABLE " + date.replace("-", "_") + "(id INT NOT NULL, "
+                            + "the_key VARCHAR(160) NOT NULL, name VARCHAR(80) NOT NULL, "
+                            + "scope VARCHAR(5) NOT NULL, qualifier VARCHAR(5) NOT NULL,"
+                            + " date VARCHAR(30) NOT NULL,"
+                            + " ncloc DECIMAL(5,1), complexity DECIMAL(4,1));");
 
             rs = stmt.execute("LOAD DATA LOCAL INFILE '//Users/weiyoud/Perforce/weiyoud_sea-weiyoud_4033/Playpen/QIC2/Archives/"
                             + date + "/17271/files.txt' INTO TABLE " + projectName + ";");// date.replace("-",
             // "_") + ";");
             rs = stmt.execute("LOAD DATA LOCAL INFILE '//Users/weiyoud/Perforce/weiyoud_sea-weiyoud_4033/Playpen/QIC2/Archives/"
                             + date + "/17271/filesHistory.txt' INTO TABLE " + projectName + "History" + ";");
+
+
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
