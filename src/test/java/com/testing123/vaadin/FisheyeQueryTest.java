50f6b72354c0bfb2afdd8acdeb122e43ad785b25
Benjamin Blumberg <blumbeb@cobalt.com>
2014-07-28 15:33:13 +0000

    new test for new methods

--- src/test/java/com/testing123/vaadin/FisheyeQueryTest.java
+++ src/test/java/com/testing123/vaadin/FisheyeQueryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,7 +31,7 @@ public class FisheyeQueryTest {
 	public void TestSimpleAuthorsQuery() throws MalformedURLException {
 		assertEquals(
 				new URL("http://fisheye.cobalt.com/rest-service-fe/search-v1/queryAsRows/Advertising.Perforce.json?query=select%20revisions%20where%20date%20in%20[2014-07-07T07:00:00,2014-07-10T07:00:00]%20and%20path%20like%20/Platform/trunk/src/main/**%20and%20path%20like%20*.java%20group%20by%20file%20return%20path,%20author,%20isDeleted"),
-				query.getAuthorsURL());
+				query.getAuthorsURL(new HashSet<String>()));
 	}
 
 
