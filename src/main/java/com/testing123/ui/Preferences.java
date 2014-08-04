a94802795713b06552eff7881c01c153666eba63
Christopher Chen <chenc@cobalt.com>
2014-08-04 23:07:51 +0000

    added faster localhost URL for the VM to use

--- src/main/java/com/testing123/ui/Preferences.java
+++ src/main/java/com/testing123/ui/Preferences.java
@@ -4,7 +4,9 @@ import com.testing123.vaadin.ConvertDate;
 
 public class Preferences {
 	
+//	public static final String DB_SERVER = "mysql://localhost:3306";		// used for VM
 	public static final String DB_SERVER = "mysql://dc2pvpdc00059.vcac.dc2.dsghost.net:3306";
+
 	public static final String DB_USER = "root";
 	public static final String DB_PASS = "password";
 	public static final String DB_NAME = "dataList4";
