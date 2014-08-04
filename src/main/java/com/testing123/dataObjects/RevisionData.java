22385177a9713d3d79ffdff156e5ec83ab934753
Benjamin Blumberg <blumbeb@cobalt.com>
2014-08-04 16:16:34 +0000

    Added revision data object for fisheye revisions

---
+++ src/main/java/com/testing123/dataObjects/RevisionData.java
@@ -0,0 +1,38 @@
+package com.testing123.dataObjects;
+
+public class RevisionData {
+
+	private String fisheyePath;
+	private int churn;
+	private String author;
+
+	public RevisionData() {
+		fisheyePath = null;
+		churn = -1;
+		this.author = null;
+	}
+
+	public RevisionData(String line) {
+		String[] split = line.split(",");
+		this.fisheyePath = split[0].replaceAll("\"", "");
+		this.churn = Integer.parseInt(split[3]) + Integer.parseInt(split[2]);
+		this.author = split[1].replaceAll("\"", "");
+	}
+
+	public String getFisheyePath() {
+		return fisheyePath;
+	}
+
+	public int getChurn() {
+		return churn;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String toString() {
+		return "RevisionData [Fisheyepath=" + fisheyePath + ", churn=" + churn + ", author=" + author + "]";
+	}
+}
---
+++ src/test/java/com/testing123/dataObjects/RevisionDataTest.java
@@ -0,0 +1,34 @@
+package com.testing123.dataObjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RevisionDataTest {
+
+	@Test
+	public void testCreatable() {
+		RevisionData rd = new RevisionData();
+		assertEquals(null,rd.getAuthor());
+		assertEquals(-1,rd.getChurn());
+		assertEquals(null,rd.getFisheyePath());
+	}
+	
+	@Test
+	public void testSampleInput(){
+		String text = "\"Websites/Tetra/trunk/modules/dealer-service/src/main/java/com/cobaltgroup/websites/dealerservice/util/LayoutXmlParser.java\",\"ghoshs\",142,9";
+		RevisionData rd = new RevisionData(text);
+		assertEquals("ghoshs",rd.getAuthor());
+		assertEquals(151,rd.getChurn());
+		assertEquals("Websites/Tetra/trunk/modules/dealer-service/src/main/java/com/cobaltgroup/websites/dealerservice/util/LayoutXmlParser.java",rd.getFisheyePath());
+	}
+	
+	@Test
+	public void testToString(){
+		String text = "\"ui/vaadin-ui/fake.java\",\"blum\",123,321";
+		RevisionData rd = new RevisionData(text);
+		assertEquals("ui/vaadin-ui/fake.java", rd.getFisheyePath());
+		assertEquals("blum", rd.getAuthor());
+		assertEquals(444,rd.getChurn());
+	}
+}
