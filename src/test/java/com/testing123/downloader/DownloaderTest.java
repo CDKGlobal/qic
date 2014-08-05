fbc0ad78cf4728e0f7082f32630cb7289dcd119b
Weiyou Dai <weiyoud@cobalt.com>
2014-08-05 22:33:11 +0000

    Test for downloader now.

--- src/test/java/com/testing123/downloader/DownloaderTest.java
+++ src/test/java/com/testing123/downloader/DownloaderTest.java
@@ -1,37 +1,44 @@
 package com.testing123.downloader;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.List;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.testing123.vaadin.WebData;
+
 public class DownloaderTest {
-	private static Downloader dl;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		dl = new Downloader();
-		File folder = new File("Tester");
-		folder.mkdir();
-	}
-	
-	@AfterClass
-	public static void cleanUpAfterClass() throws Exception {
-		File f = new File("Tester");
-		delete(f);
-	}
-	
-	public static void delete(File f) throws Exception {
-		if (f.isDirectory()) {
-			for (File c : f.listFiles())
-				delete(c);
-		}
-		if (!f.delete()) {
-		    throw new FileNotFoundException("Failed to delete file: " + f);
-		}
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        /*
+         * dl = new Downloader();
+         * File folder = new File("Tester");
+         * folder.mkdir();
+         */
+    }
+
+    @Test
+    public void TestDownloadProjectsAndStoreInList() {
+        List<WebData> projectList = Downloader.downloadProjectsAndStoreInList();
+        assertEquals("com.onstation:uploader", projectList.get(0).getKey());
+    }
+    /*
+     * public static void delete(File f) throws Exception {
+     * if (f.isDirectory()) {
+     * for (File c : f.listFiles()) {
+     * delete(c);
+     * }
+     * }
+     * if (!f.delete()) {
+     * throw new FileNotFoundException("Failed to delete file: " + f);
+     * }
+     * }
+     * @AfterClass
+     * public static void cleanUpAfterClass() throws Exception {
+     * File f = new File("Tester");
+     * delete(f);
+     * }
+     */
 }
