e50be7156f705194a9f2dd45f54aaad0a79db69c
Benjamin Blumberg <blumbeb@cobalt.com>
2014-08-05 18:53:18 +0000

    removed unused method calls

--- src/main/java/com/testing123/downloader/DataDownloaderMain.java
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -1,20 +1,16 @@
 package com.testing123.downloader;
 
 import java.sql.Connection;
-import java.util.List;
 
-import com.testing123.vaadin.WebData;
 
 public class DataDownloaderMain {
     public static void main(String[] args) {
         try {
-            Downloader dl = new Downloader();
-            // downloads everything from the platform project
-            List<WebData> projectList = Downloader.downloadProjectsAndStoreInList();
             DatabaseConnector dbConnector = new DatabaseConnector();
             // builds a connection
             Connection conn = DatabaseConnector.getConnection();
             dbConnector.createDbAndLoadTableForProject(conn);
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
