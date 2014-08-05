020b33370090544aa1368fd12357b548eb7f88c1
Weiyou Dai <weiyoud@cobalt.com>
2014-08-05 18:33:23 +0000

    Delete lines of comments

--- src/main/java/com/testing123/downloader/DataDownloaderMain.java
+++ src/main/java/com/testing123/downloader/DataDownloaderMain.java
@@ -1,39 +1,22 @@
 package com.testing123.downloader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+
+import com.testing123.vaadin.WebData;
 
 public class DataDownloaderMain {
     public static void main(String[] args) {
         try {
             Downloader dl = new Downloader();
             // downloads everything from the platform project
-            ArrayList<Integer> idList = formerIdList();
-            dl.downloadProjects("platform", idList);
+            List<WebData> projectList = Downloader.downloadProjectsAndStoreInList();
             DatabaseConnector dbConnector = new DatabaseConnector();
-            String todayDate = Downloader.today;
             // builds a connection
-            Connection conn = dbConnector.getConnection();
-            dbConnector.createDbAndLoadTable(conn, todayDate, "platform");
+            Connection conn = DatabaseConnector.getConnection();
+            dbConnector.createDbAndLoadTableForProject(conn);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-
-    public static ArrayList<Integer> formerIdList() throws FileNotFoundException {
-        ArrayList<Integer> idList = new ArrayList<Integer>();
-        Scanner input = new Scanner(new File("src/main/java/com/testing123/downloader/files0723.txt"));
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            Scanner lineScanner = new Scanner(line);
-            int id = lineScanner.nextInt();
-            idList.add(id);
-        }
-        return idList;
-
     }
 }
