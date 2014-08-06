e8152effaa157ad6dc72a2bea94f07c0d6da367f
Weiyou Dai <weiyoud@cobalt.com>
2014-08-06 17:41:34 +0000

    Runnable fisheyeuploader to uploader the data from fisheye to database

--- src/main/java/com/testing123/downloader/FisheyeUploader.java
+++ src/main/java/com/testing123/downloader/FisheyeUploader.java
@@ -9,23 +9,21 @@ import com.testing123.dataObjects.ChangedData;
 
 public class FisheyeUploader {
 
-    public Connection getConnection() {
-        return DatabaseConnector.getConnection();
-    }
 
-    public List<ChangedData> getFisheyeList() {
-        return new DownloadFisheyeData().getAllFisheyeUpdates();
-    }
 
-    public void uploadFEToDatabase(Connection conn, List<ChangedData> fisheyeDataList) {
+    public void uploadFEToDatabase(Connection conn) {
+        List<ChangedData> fisheyeDataList = new DownloadFisheyeData().getAllFisheyeUpdates();
         Statement stmt = null;
         try {
             stmt = conn.createStatement();
             for (ChangedData fisheyeData : fisheyeDataList) {
-
+                stmt.execute("UPDATE allFileHistory3 SET "
+                                + "churn = '" + fisheyeData.getChurn() + "', "
+                                + "authors = '" + fisheyeData.getAuthors() + "' "
+                                + "WHERE file_key = '" + fisheyeData.getFile_key() + "' "
+                                + "and dbdate = '" + fisheyeData.getDate() + "';");
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
