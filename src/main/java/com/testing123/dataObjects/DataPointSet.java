57515f9692bc6f11bee263c0e082874afd54557b
Christopher Chen <chenc@cobalt.com>
2014-08-07 22:23:29 +0000

    used for colors

---
+++ src/main/java/com/testing123/dataObjects/DataPointSet.java
@@ -0,0 +1,31 @@
+package com.testing123.dataObjects;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.testing123.ui.Preferences;
+
+public class DataPointSet {
+	private Set<DataPoint> negVals;
+	private Set<DataPoint> posVals;
+	
+	public DataPointSet() {
+		this.negVals = new HashSet<DataPoint>();
+		this.posVals = new HashSet<DataPoint>();
+	}
+	
+	public void addNeg(DataPoint pt) {
+		negVals.add(pt);
+	}
+	
+	public void addPos(DataPoint pt) {
+		posVals.add(pt);
+	}
+	
+	public String toString() {
+		return "[ { data: " + negVals + ", points: { show: true, fill: true, fillColor: \"" + 
+				Preferences.FILL_COLOR_NEG + "\" } }, " + 
+				"{ data: " + posVals + ", points: { show: true, fill: true, fillColor: \"" + 
+				Preferences.FILL_COLOR_POS + "\" }} ]";
+	}
+}
