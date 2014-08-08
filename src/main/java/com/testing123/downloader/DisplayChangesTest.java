ed8c2910438a6115c6793577e405b262c404641e
Benjamin Blumberg <blumbeb@cobalt.com>
2014-08-08 20:51:37 +0000

    Got rid of mock interfaces, instead use mockito

--- src/main/java/com/testing123/downloader/DisplayChangesTest.java
+++ src/main/java/com/testing123/downloader/DisplayChangesTest.java
@@ -10,11 +10,11 @@ public class DisplayChangesTest {
 
 	@Test
 	public void test() {
-//		ConvertDate startDate = new ConvertDate("2014-08-05");
-//		ConvertDate endDate = new ConvertDate("2014-08-07");
-//		String dbPath = "com.vaadin:QIC2:src/main/java/com/testing123/controller/UIState.java";
-//		new DisplayChanges().fisheyeRevision(dbPath, startDate, endDate);
-//		System.out.println("done");
+		ConvertDate startDate = new ConvertDate("2014-08-05");
+		ConvertDate endDate = new ConvertDate("2014-08-07");
+		String dbPath = "com.vaadin:QIC2:src/main/java/com/testing123/controller/UIState.java";
+		new DisplayChanges().fisheyeRevision(dbPath, startDate, endDate);
+		System.out.println("done");
 	}
 	
 	@Test
--- src/test/java/com/testing123/downloader/DownloadFisheyeDataTest.java
+++ src/test/java/com/testing123/downloader/DownloadFisheyeDataTest.java
@@ -4,33 +4,59 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.testing123.dataObjects.ChangedData;
+import com.testing123.dataObjects.ConvertProject;
 import com.testing123.dataObjects.RevisionData;
+import com.testing123.vaadin.DatabaseInterface;
 import com.testing123.vaadin.RegexUtil;
 
 public class DownloadFisheyeDataTest {
 	
 	private static List<ChangedData> test;
+	private FisheyeInterface FIMock;
+	private DatabaseInterface DBIMock;
 	
 	@Before
-	public void onlyOnce(){
-		FisheyeInterface FIMock = Mockito.mock(FisheyeInterface.class);
+	public void before(){	
+		FIMock = Mockito.mock(FisheyeInterface.class);
+		DBIMock = Mockito.mock(DatabaseInterface.class);
+	}
+
+
+	@Test
+	public void test(){
+		
 		Set<RevisionData> set1 = getSet1();
 		Mockito.when(FIMock.getRevisionsFromProject("QIC.Perforce", "Mock")).thenReturn(set1);
 		Mockito.when(FIMock.getRevisionsFromProject("QIC.Perforce", "MockEmpty")).thenReturn(new HashSet<RevisionData>());
-		test = new DownloadFisheyeData(FIMock, new MockDBI()).getAllFisheyeUpdates();
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("this.is.one.type.of.path", 18);
+		map.put("this/is/the/other/type.java", 21);
+		List<ConvertProject> list = new LinkedList<ConvertProject>();
+		list.add(new ConvertProject("Unused", "alsoUnused", 0, "/QIC/Mock"));
+		list.add(new ConvertProject("NoNameHere", "NoKeyEvenNeeded", 42542, "/QIC/MockEmpty"));
+		Mockito.when(DBIMock.getMapToID(0)).thenReturn(map);
+		Mockito.when(DBIMock.getAvailableProjects()).thenReturn(list);
+		
+		test = new DownloadFisheyeData(FIMock, DBIMock).getAllFisheyeUpdates();
+		
+		assertEquals(2,test.size());
+		ChangedData meMyselfIrine = new ChangedData(21, "2014-8-8", 76, "[Me, Irene, Myself]");
+		ChangedData chris = new ChangedData(18,"2014-8-8",13,"[Chris]");
+		assertTrue(test.contains(meMyselfIrine));
+		assertTrue(test.contains(chris));
 	}
-
+	
 	private Set<RevisionData> getSet1() {
 		Set<RevisionData> set = new HashSet<RevisionData>();
 		add(set,"\"/Different/useless/starter/this/is/the/other/type.java\",\"Me\",0,13");
@@ -41,15 +67,6 @@ public class DownloadFisheyeDataTest {
 		add(set,"\"/Useless/starter/this/is/one/type/of/path.java\",\"Chris\",0,13");
 		add(set,"\"path/doesnt/match\",\"Weiyou\",0,13");
 		return set;
-	}
-
-	@Test
-	public void test(){
-		assertEquals(2,test.size());
-		ChangedData meMyselfIrine = new ChangedData(21, "2014-8-8", 76, "[Me, Irene, Myself]");
-		ChangedData chris = new ChangedData(18,"2014-8-8",13,"[Chris]");
-		assertTrue(test.contains(meMyselfIrine));
-		assertTrue(test.contains(chris));
 	}
 
 	
