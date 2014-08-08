b20f832a4723ad2998ff41e7ab53b9dab3389f81
Weiyou Dai <weiyoud@cobalt.com>
2014-08-08 15:32:14 +0000

    fixed small error

--- src/main/java/com/testing123/downloader/FisheyeUploader.java
+++ src/main/java/com/testing123/downloader/FisheyeUploader.java
@@ -10,7 +10,6 @@ import com.testing123.dataObjects.ChangedData;
 public class FisheyeUploader {
 
 
-
     public void uploadFEToDatabase(Connection conn) {
         List<ChangedData> fisheyeDataList = new DownloadFisheyeData().getAllFisheyeUpdates();
         Statement stmt = null;
@@ -18,11 +17,16 @@ public class FisheyeUploader {
             stmt = conn.createStatement();
             for (ChangedData fisheyeData : fisheyeDataList) {
                 stmt.execute("UPDATE allFileHistory3 SET "
-                                + "churn = '" + fisheyeData.getChurn() + "', "
+                                + "churn = " + fisheyeData.getChurn() + ", "
                                 + "authors = '" + fisheyeData.getAuthors() + "' "
-                                + "WHERE file_key = '" + fisheyeData.getFile_key() + "' "
+                                + "WHERE file_id = " + fisheyeData.getFile_id() + " "
                                 + "and dbdate = '" + fisheyeData.getDate() + "';");
+                System.out.println("churn = '" + fisheyeData.getChurn() + "', "
+                                + "authors = '" + fisheyeData.getAuthors() +
+                                "file_id = " + fisheyeData.getFile_id() + " "
+                                + "and dbdate = '" + fisheyeData.getDate());
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
