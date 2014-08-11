154629c9afd23dc8cea721e23e35c8c1a6eba79c
Benjamin Blumberg <blumbeb@cobalt.com>
2014-08-11 16:20:13 +0000

    add tests for FisheyeQuery

---
+++ src/test/java/com/testing123/downloader/FisheyeQueryTest.java
@@ -0,0 +1,19 @@
+package com.testing123.downloader;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FisheyeQueryTest {
+
+	@Test
+	public void test() {
+		new FisheyeQuery().getRevisionsFromProject("Advertising.Perforce", "Platform");
+	}
+	
+	@Test
+	public void TestPopUp(){
+		new FisheyeQuery().popUpChangesInFisheye("QIC.Perforce", "src/main/java/com/testing123/downloader/DBDeltaCalculator.java", 806170, 806354);
+	}
+
+}
