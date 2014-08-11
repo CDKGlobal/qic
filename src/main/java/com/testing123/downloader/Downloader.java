972af8e3b768f8ce96948f0fc6854ea8c4920027
Weiyou Dai <weiyoud@cobalt.com>
2014-08-11 23:01:52 +0000

    moved method to here from DatabaseConnector

--- src/main/java/com/testing123/downloader/Downloader.java
+++ src/main/java/com/testing123/downloader/Downloader.java
@@ -1,9 +1,13 @@
 package com.testing123.downloader;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testing123.vaadin.WebData;
 
@@ -11,7 +15,6 @@ import com.testing123.vaadin.WebData;
  * Downloader downloads all the metric data available from Sonar at the time of execution.  To use,
  * make an instance of this class, and call downloadProject().
  * 
- * @author chenc
  */
 public class Downloader {
 
@@ -27,7 +30,7 @@ public class Downloader {
      * @param projectName the project name
      */
 
-    public static List<WebData> downloadProjectsAndStoreInList(boolean... test) {
+    public static List<WebData> downloadProjectsAndStoreInList() {
         String projectLink = "http://sonar.cobalt.com/api/resources?&depth=0&metrics=ncloc,complexity&format=json";
         URL projectURL;
         try {
@@ -41,4 +44,13 @@ public class Downloader {
         }
         return null;
     }
+
+    public static List<WebData> parseData(WebData project, int depth) throws MalformedURLException, IOException, JsonParseException, JsonMappingException {
+        String projectOwnLink = "http://sonar.cobalt.com/api/resources?resource=" + project.getKey() + "&depth=" + depth + "&metrics=ncloc,complexity&format=json";
+        URL projectOwnURL = new URL(projectOwnLink);
+        List<WebData> fileList = mapper.readValue(projectOwnURL, new TypeReference<List<WebData>>() {
+        });
+        return fileList;
+    }
+
 }
