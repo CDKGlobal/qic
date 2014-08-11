245bb6c80e3ab4f2a7e5d53b36c329b7a5c840e5
Christopher Chen <chenc@cobalt.com>
2014-08-11 21:57:50 +0000

    updated to support new files

--- src/main/java/com/testing123/downloader/DBDataCalculatorShort.java
+++ src/main/java/com/testing123/downloader/DBDataCalculatorShort.java
@@ -49,8 +49,15 @@ public class DBDataCalculatorShort extends DBDeltaCalculator {
 				}
 				conn2.close();
 			}
-			conn.updateQuery("UPDATE allFileHistory3 SET delta_complexity = 0 WHERE dbdate = '" 
+
+			st = c.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+	                ResultSet.CONCUR_UPDATABLE);
+			rs = st.executeQuery("UPDATE allFileHistory3 SET delta_complexity = 0 WHERE dbdate = '" 
 					+ DataSupportMain.getFrmtDate(new DateTime()) + "' AND delta_complexity IS NULL AND complexity != -1;");
+			while (rs.next()) {
+				rs.updateDouble("delta_complexity", rs.getDouble("complexity"));
+	            rs.updateRow();
+			}
 			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
