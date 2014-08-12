656f1e5342f00d4459158ce1654e18a0cced05cd
Weiyou Dai <weiyoud@cobalt.com>
2014-08-12 23:16:12 +0000

    added test for path

--- src/test/java/com/testing123/downloader/DatabaseConnectorTest.java
+++ src/test/java/com/testing123/downloader/DatabaseConnectorTest.java
@@ -1,6 +1,11 @@
 package com.testing123.downloader;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,17 +15,31 @@ import org.junit.Test;
 public class DatabaseConnectorTest {
     private static Connection conn;
 
+    private static Statement stmt;
+
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        conn = DatabaseConnector.getConnection();
-
+        conn = DriverManager.getConnection("jdbc:mysql://dc2pvpdc00059.vcac.dc2.dsghost.net:3306/TestDatabase?" +
+                        "user=root&password=password");
+        stmt = conn.createStatement();
+        Boolean rs = false;
+        rs = stmt.execute("DELETE FROM allFileHistory3");
+        /*
+         * rs).executeQuery(
+         * "INSERT INTO allFileHistory3 (file_id, file_key, name, ncloc, complexity, dbdate,delta_complexity, churn, authors) "
+         * + "VALUES (1111111, 'file_key', 'name', 5.0, 5.0, '2014-08-10', 5.0, 6, '[weiyoud, chena]');");
+         */
     }
+
 
     @Test
     public void TestGetConnection() {
-        Connection conn = DatabaseConnector.getConnection();
+        Connection connection = DatabaseConnector.getConnection();
         try {
-            Statement stmt = conn.createStatement();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM allFileHistory3");
+            assertTrue(rs.next());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,11 +47,23 @@ public class DatabaseConnectorTest {
 
     /*
      * @Test
-     * public void TestGetProjectPath() {
-     * System.out.println("path = " + DatabaseConnector.getProjectPath(17271));
-     * assertEquals("/Advertising/Platform/trunk/", DatabaseConnector.getProjectPath(17271));
+     * public void TestWriteToTxtFileAndUpsertMetrics() {
+     * try {
+     * Boolean b = stmt.execute(
+     * "INSERT INTO allFileHistory3 (file_id, file_key, name, ncloc, complexity, dbdate,delta_complexity, churn, authors) "
+     * + "VALUES (1111111, 'file_key', 'name', 5.0, 5.0, '2014-08-10', 5.0, 6, '[weiyoud, chena]');");
+     * ResultSet rs =
+     * stmt.executeQuery("SELECT * FROM allFileHistory3 where file_id = 1111111, file_key = 'file_key';");
+     * assertTrue(rs.next());
+     * } catch (SQLException e) {
+     * e.printStackTrace();
+     * }
      * }
      */
+    @Test
+    public void TestGetProjectPath() {
+        assertEquals("/Advertising/platform/trunk/", DatabaseConnector.getProjectPath(17271));
+    }
 
 
 }
