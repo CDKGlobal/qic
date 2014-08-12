fed1574624b97d31541f8f759bb4b287c66885e2
Weiyou Dai <weiyoud@cobalt.com>
2014-08-12 22:17:05 +0000

    condensed to one method

--- src/main/java/com/testing123/downloader/Downloader.java
+++ src/main/java/com/testing123/downloader/Downloader.java
@@ -1,18 +1,14 @@
 package com.testing123.downloader;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testing123.vaadin.WebData;
 
 /**
- * Downloader downloads all the metric data available from Sonar at the time of execution.  To use,
+ * To use,
  * make an instance of this class, and call downloadProject().
  * 
  */
@@ -25,32 +21,21 @@ public class Downloader {
     }
 
     /**
-     * Downloads all the metric data (including package/directory/file data) available
-     * 
      * @param projectName the project name
      */
 
-    public static List<WebData> downloadProjectsAndStoreInList() {
-        String projectLink = "http://sonar.cobalt.com/api/resources?&depth=0&metrics=ncloc,complexity&format=json";
-        URL projectURL;
+    public static List<WebData> downloadAndStoreInList(String projectKey, int depth, String distinguisher) {
         try {
-            projectURL = new URL(projectLink);
-
-            List<WebData> projectList = mapper.readValue(projectURL, new TypeReference<List<WebData>>() {
+            String link = "http://sonar.cobalt.com/api/resources" + distinguisher + projectKey + "&depth=" + depth + "&metrics=ncloc,complexity&format=json";
+            URL ownURL = new URL(link);
+            System.out.println("ok");
+            System.out.println(link);
+            List<WebData> list = mapper.readValue(ownURL, new TypeReference<List<WebData>>() {
             });
-            return projectList;
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-
-    public static List<WebData> parseData(WebData project, int depth) throws MalformedURLException, IOException, JsonParseException, JsonMappingException {
-        String projectOwnLink = "http://sonar.cobalt.com/api/resources?resource=" + project.getKey() + "&depth=" + depth + "&metrics=ncloc,complexity&format=json";
-        URL projectOwnURL = new URL(projectOwnLink);
-        List<WebData> fileList = mapper.readValue(projectOwnURL, new TypeReference<List<WebData>>() {
-        });
-        return fileList;
-    }
-
 }
