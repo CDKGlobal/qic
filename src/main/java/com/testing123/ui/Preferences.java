51bae61a343f473fb0286b92469d0243689adf2b
Weiyou Dai <weiyoud@cobalt.com>
2014-08-13 17:52:52 +0000

    added DB_NAME_TEST for test

--- src/main/java/com/testing123/ui/Preferences.java
+++ src/main/java/com/testing123/ui/Preferences.java
@@ -3,33 +3,34 @@ package com.testing123.ui;
 import com.testing123.dataObjects.ConvertDate;
 
 public class Preferences {
-	
-//	public static final String DB_SERVER = "mysql://localhost:3306";		// used for VM
-	public static final String DB_SERVER = "mysql://dc2pvpdc00059.vcac.dc2.dsghost.net:3306";
-	
-//	public static final String URL = "localhost:8080/QIC2";
-	public static final String URL = "qic.cobalt.com:8080";
-	
-	public static final String DB_USER = "root";
-	public static final String DB_PASS = "password";
-	public static final String DB_NAME = "dataList4";
-	public static final String PROJECT_TABLE = "projectList";
-	public static final String STATIC_TABLE = "allFileList"; 
-	public static final String DATA_TABLE = "allFileHistory";
-	
-	public static final ConvertDate DEFAULT_START_DATE = new ConvertDate("2014-07-29");
-	public static final ConvertDate DEFAULT_END_DATE = new ConvertDate("2014-07-30");
-	
-	public static final String GRPAH_COLOR = "#033F8D";
-	public static final String FILL_COLOR_POS = "#FF0000";
-	public static final String FILL_COLOR_NEG = "#009933";
-	
-	
-	public static final String[] FISHEYE_REPOS = new String[] {
-		"Advertising.Perforce", "Core.Perforce", "Intelligence.Perforce", "OpenPlatform.Perforce", 
-		"Owner.Perforce", "ProfessionalServices.Perforce", "ReleaseEngineering.Perforce",
-		"Social.Perforce", "QIC.Perforce" };
-	
-	public static final String FISHEYE_HOME = "http://fisheye.cobalt.com";
+
+    //	public static final String DB_SERVER = "mysql://localhost:3306";		// used for VM
+    public static final String DB_SERVER = "mysql://dc2pvpdc00059.vcac.dc2.dsghost.net:3306";
+
+    //	public static final String URL = "localhost:8080/QIC2";
+    public static final String URL = "qic.cobalt.com:8080";
+
+    public static final String DB_USER = "root";
+    public static final String DB_PASS = "password";
+    public static final String DB_NAME = "dataList4";
+    public static final String DB_NAME_TEST = "TestDatabase";
+    public static final String PROJECT_TABLE = "projectList";
+    public static final String STATIC_TABLE = "allFileList";
+    public static final String DATA_TABLE = "allFileHistory";
+
+    public static final ConvertDate DEFAULT_START_DATE = new ConvertDate("2014-07-29");
+    public static final ConvertDate DEFAULT_END_DATE = new ConvertDate("2014-07-30");
+
+    public static final String GRPAH_COLOR = "#033F8D";
+    public static final String FILL_COLOR_POS = "#FF0000";
+    public static final String FILL_COLOR_NEG = "#009933";
+
+
+    public static final String[] FISHEYE_REPOS = new String[] {
+        "Advertising.Perforce", "Core.Perforce", "Intelligence.Perforce", "OpenPlatform.Perforce",
+        "Owner.Perforce", "ProfessionalServices.Perforce", "ReleaseEngineering.Perforce",
+        "Social.Perforce", "QIC.Perforce" };
+
+    public static final String FISHEYE_HOME = "http://fisheye.cobalt.com";
 
 }
