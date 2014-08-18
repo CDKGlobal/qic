60b685123c8983ad5c20ad400dcdaf683c076b89
Benjamin Blumberg <blumbeb@cobalt.com>
2014-08-18 16:07:48 +0000

    added testing for second util method

--- src/test/java/com/testing123/vaadin/RegexUtilTest.java
+++ src/test/java/com/testing123/vaadin/RegexUtilTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.testing123.dataObjects.RevisionData;
-
 public class RegexUtilTest {
 	
 	@Test
@@ -24,6 +22,25 @@ public class RegexUtilTest {
 		String text = "\"Websites/Tetra/trunk/modules/dealer-service/src/main/java/com/cobaltgroup/websites/dealerservice/util/LayoutXmlParser.java\",142,9";
 		assertFalse(RegexUtil.isRevisionData(text));
 
+	}
+	
+	@Test
+	public void testNullDate(){
+		assertFalse(RegexUtil.isCorrectDateFormat(null));
+	}
+	
+	@Test
+	public void testIncorrectDates(){
+		assertFalse(RegexUtil.isCorrectDateFormat("01-01-2014"));
+		assertFalse(RegexUtil.isCorrectDateFormat("2014/01/10"));
+		assertFalse(RegexUtil.isCorrectDateFormat("1111-13-01"));
+		assertFalse(RegexUtil.isCorrectDateFormat("1111-00-00"));
+
+	}
+	
+	@Test
+	public void testCorrectDateFormat(){
+		assertTrue(RegexUtil.isCorrectDateFormat("2014-12-30"));
 	}
 
 }
