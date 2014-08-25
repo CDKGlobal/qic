185fe23ddd8eb82bf7e829fb59f2d6c986a6345d
Christopher Chen <chenc@cobalt.com>
2014-08-25 16:59:10 +0000

    changed unused imports

--- src/test/java/com/testing123/controller/AvailableResourceTest.java
+++ src/test/java/com/testing123/controller/AvailableResourceTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.testing123.dataObjects.ConvertDate;
